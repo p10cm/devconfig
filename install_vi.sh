029d5a52abd914ef3a41c4f1b0b56adf4c52ce2f
min <pjm0329@gmail.com>
2015-11-11 21:35:01 +0900

    fist commit

---
+++ install_vi.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ -e ~/.vimrc -o -e ~/.vim ]; then
+	echo "경고: 설치를 진행하려면 ~/.vim/ 디렉토리와 ~/.vimrc 파일을 삭제해야 합니다."
+	exit
+fi
+
+echo "심볼릭 릭크 생성..."
+cp ~/devconfig/vim/vimrc ~/.vimrc
+
+echo "vundle 다운로드중..."
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+echo "solarized 설치중..."
+cp -r ~/devconfig/theme-solarized/vim-colors-solarized/colors/ ~/.vim/
+
+echo "plugin 설치..."
+cp -r ~/devconfig/vim/plugins ~/.vim/bundle/
+
+echo "vundle 설치중..."
+vi -c :BundleInstall -c :qa
